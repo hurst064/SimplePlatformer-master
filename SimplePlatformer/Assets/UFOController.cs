1b9fd6e0ae3a4411751ca030d95282db5fb80cc2
Lexie <hurst064@minidokaschools.org>
2019-04-16 10:30:05 -0600

    new code

--- SimplePlatformer/Assets/UFOController.cs
+++ SimplePlatformer/Assets/UFOController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class UFOController : MonoBehaviour {
 
-	//Add Variables here
-
+    //Add Variables here
+    public Rigidbody2D theRB2d;
+    public float movementSpeed;
 
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        theRB2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -20,18 +22,22 @@ public class UFOController : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("you pressed the d key");
+            theRB2d.velocity += new Vector2(1.0f,0.0f)*movementSpeed;
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("you pressed the w key");
+            theRB2d.velocity += new Vector2(0f, 1.0f)*movementSpeed;
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("you pressed the a key");
+            theRB2d.velocity += new Vector2(-1.0f,0.0f)*movementSpeed;
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             Debug.Log("you pressed the s key");
+            theRB2d.velocity += new Vector2(0.0f,-1.0f)*movementSpeed;
         }
 
     }
