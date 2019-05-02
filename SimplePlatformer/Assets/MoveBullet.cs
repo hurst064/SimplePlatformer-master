b836876c2e32fb5009a2dec59d0af58d304c715c
Lexie <hurst064@minidokaschools.org>
2019-05-02 11:08:10 -0600

    update

---
+++ SimplePlatformer/Assets/MoveBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour {
+    public float movementSpeed;
+        public Rigidbody2D theRB2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB2D.velocity = transform.right * movementSpeed;
+    }
+    void OnBecomeIvisible()
+    {
+        Destroy(gameObject);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
