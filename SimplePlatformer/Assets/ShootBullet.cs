083193053cc0f7a452697ad5285a9eefb95329f0
Lexie <hurst064@minidokaschools.org>
2019-05-06 11:04:34 -0600

    lasers

--- SimplePlatformer/Assets/ShootBullet.cs
+++ SimplePlatformer/Assets/ShootBullet.cs
@@ -6,7 +6,7 @@ public class ShootBullet : MonoBehaviour{
 
 
     public GameObject Barrel;
-    public laserSiteControll lzrSight;
+    public LaserSiteControll lzrSight;
     public float speed;
     public float angle;
 
@@ -26,14 +26,15 @@ public class ShootBullet : MonoBehaviour{
 
     public void ShootProjectile()
     {
-        Vector3 dir = Input.mousePosition -
-  Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition -  Camera.main.WorldToScreenPoint(transform.position);
+       // Vector3 dir = Input.mousePosition;
         angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        barrel.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Barrel.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         dir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         dir.z = 0f;
-        lzrSight.SetEnd(dir):
+        //lzrSight.SetEnd(dir);
+        lzrSight.SetEnd(dir);
 
         if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.Z))
         {
@@ -42,12 +43,11 @@ public class ShootBullet : MonoBehaviour{
 
         if (Input.GetMouseButtonDown(0))
         {
-            dir.= Input.mousePosition -
-            Camera.main.WorldToScreenPoint(Transform.position);
+            dir= Input.mousePosition -Camera.main.WorldToScreenPoint(transform.position);
             angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
-            barrel.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            Instantiate(ShootProjectile, transform.position, barrel.transform.rotaion);
+            Barrel.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            Instantiate(projectile, transform.position, Barrel.transform.rotation);
         }
     }
 }
