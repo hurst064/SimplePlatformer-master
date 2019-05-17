f8a92ce453e97ba224b851a65ea195ec17647046
Lexie <hurst064@minidokaschools.org>
2019-05-17 11:10:26 -0600

    up

---
+++ SimplePlatformer/Assets/LevelTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelTimer : MonoBehaviour
+{
+    public Text levelTime;
+    public float timeCounter;
+    public string LeveltoLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter = timeCounter - Time.deltaTime;
+        levelTime.text = Mathf.RoundToInt(timeCounter).ToString();
+        if (timeCounter<0)
+        {
+            //restart the level
+            LoadLevel(LeveltoLoad);
+        }
+    }
+
+
+    public void LoadLevel(string  levelName)
+    {
+       
+            SceneManager.LoadScene(levelName);
+
+    }
+}
