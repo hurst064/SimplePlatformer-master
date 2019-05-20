c4ebfda70909e9d856c15ee08e1e85abf870a212
Lexie <hurst064@minidokaschools.org>
2019-05-20 10:23:00 -0600

    up

---
+++ SimplePlatformer/Assets/PlayerHealthControll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealthControll : MonoBehaviour
+{
+    public int health;
+    public string leveltoload;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            //gameObject.transform.parent.gameObject.SetActive(false);
+            SceneManager.LoadScene(leveltoload);
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        health = health - damage;
+    }
+}
